49851810e5c48186e0dc6a2ee1969a1eed1f6743
taemin19 <daniel.thebault19@gmail.com>
2018-05-04 13:47:23 +0200

    Implement a method to get a single product

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -17,11 +17,33 @@ class ProductsController extends Controller
      */
     public function listAction()
     {
-        $users = $this->getDoctrine()
+        $products = $this->getDoctrine()
             ->getRepository('App:Product')
             ->findAll();
 
-        $data = $this->get('serializer')->serialize($users, 'json', ['groups' => ['list']]);
+        $data = $this->get('serializer')->serialize($products, 'json', ['groups' => ['list']]);
+
+        return JsonResponse::fromJsonString($data);
+    }
+
+    /**
+     * @param int $id
+     * @return JsonResponse
+     *
+     * @Route("/products/{id}", name="products_show")
+     * @Method("GET")
+     */
+    public function showAction(int $id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('App:Product')
+            ->find($id);
+
+        if (empty($product)) {
+            return new JsonResponse(['message' => 'Not found.'], 404);
+        }
+
+        $data = $this->get('serializer')->serialize($product, 'json', ['groups' => ['product']]);
 
         return JsonResponse::fromJsonString($data);
     }
