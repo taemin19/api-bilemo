791b01a2d409a6cb9b694e4a4b8e36b5091d5c48
taemin19 <daniel.thebault19@gmail.com>
2018-05-11 21:27:14 +0200

    Implement a method to create an user

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class UsersController extends Controller
 {
@@ -66,5 +69,33 @@ class UsersController extends Controller
         $em->flush();
 
         return new JsonResponse(null, 204);
+    }
+
+    /**
+     * @return JsonResponse
+     *
+     * @Route("/users", name="users_new")
+     * @Method("POST")
+     */
+    public function new(Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $user = new User();
+        $user->setFirstname($data['firstname']);
+        $user->setLastname($data['lastname']);
+        $user->setEmail($data['email']);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $userUrl = $this->generateUrl(
+            'users_show',
+            ['id' => $user->getId()],
+            UrlGeneratorInterface::ABSOLUTE_URL
+        );
+
+        return new JsonResponse($data, 201, ['Location' => $userUrl]);
     }
 }
