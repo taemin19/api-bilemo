cc004dde0a148ced1e7b2e385191dd6416be2bae
taemin19 <daniel.thebault19@gmail.com>
2018-05-15 15:18:28 +0200

    Create the ApiExceptionSubscriber to return an ApiProblem JSON

---
+++ src/EventSubscriber/ApiExceptionSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Exception\ApiProblemException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ApiExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $e = $event->getException();
+
+        if (!$e instanceof ApiProblemException) {
+            return;
+        }
+
+        $apiProblem = $e->getApiProblem();
+
+        $response = new JsonResponse(
+            $apiProblem->toArray(),
+            $apiProblem->getStatusCode()
+        );
+        $response->headers->set('Content-Type', 'application/problem+json');
+
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            KernelEvents::EXCEPTION => 'onKernelException'
+        );
+    }
+}
