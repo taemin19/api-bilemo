c459a27edc7c7f0c8bbb80da90dfa5ed6e809e54
taemin19 <daniel.thebault19@gmail.com>
2018-05-16 17:16:07 +0200

    Add custom context to make scenario test work independently

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Entity\Product;
+use App\Entity\User;
 use Behat\Behat\Context\Context;
+use Behat\Gherkin\Node\TableNode;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
 use Symfony\Component\HttpKernel\KernelInterface;
 
 class FeatureContext implements Context
@@ -13,5 +17,56 @@ class FeatureContext implements Context
     public function __construct(KernelInterface $kernel)
     {
         $this->kernel = $kernel;
+    }
+
+    /**
+     * @BeforeScenario
+     */
+    public function clearData()
+    {
+        $purger = new ORMPurger($this->kernel->getContainer()->get('doctrine')->getManager());
+        $purger->setPurgeMode(ORMPurger::PURGE_MODE_TRUNCATE);
+        $purger->purge();
+    }
+
+    /**
+     * @Given the following products exist:
+     */
+    public function  theFollowingProductsExist(TableNode $table)
+    {
+        $em = $this->kernel->getContainer()->get('doctrine')->getManager();
+
+        foreach ($table->getHash() as $productHash) {
+            $product = new Product();
+            $product->setModel($productHash['model']);
+            $product->setBrand($productHash['brand']);
+            $product->setDescription($productHash['description']);
+            $product->setStorage($productHash['storage']);
+            $product->setColor($productHash['color']);
+            $product->setPrice($productHash['price']);
+
+            $em->persist($product);
+        }
+
+        $em->flush();
+    }
+
+    /**
+     * @Given the following users exist:
+     */
+    public function  theFollowingUsersExist(TableNode $table)
+    {
+        $em = $this->kernel->getContainer()->get('doctrine')->getManager();
+
+        foreach ($table->getHash() as $productHash) {
+            $product = new User();
+            $product->setFirstname($productHash['firstname']);
+            $product->setLastname($productHash['lastname']);
+            $product->setEmail($productHash['email']);
+
+            $em->persist($product);
+        }
+
+        $em->flush();
     }
 }
